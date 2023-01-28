1c0e3904ea9b3776419087ad07529f1c9f5205a6
ABSanthosh <a.b.santhosh02@gmail.com>
2023-01-28 10:06:26 +0530

    Project Init

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import "../styles/root/globals.scss";
+
+import { AuthProvider, getUserFromSession } from "../context/authContext";
+import App from "next/app";
+import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
+
+function MyApp({ Component, pageProps, user }) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return (
+    <AuthProvider ssrUser={user}>
+      <Head>
+        <title>Daintree</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NextNProgress color="#ff4f46" height={5} />
+      {getLayout(<Component {...pageProps} />)}
+    </AuthProvider>
+  );
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  if (appContext.router.isSsr === undefined) {
+    const appProps = await App.getInitialProps(appContext);
+    const user = await getUserFromSession(appContext.ctx);
+    return { ...appProps, user: user };
+  } else {
+    const appProps = await App.getInitialProps(appContext);
+    return { ...appProps };
+  }
+};
+
+export default MyApp;
